245dcfe22ac53fec70b8ab8634ee6f3b29b7ad13
neuman <owiroadams@gmail.com>
2023-12-12 12:16:59 +0300

    completed

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2
+
+const random =10;
+
+Math.floor(Math.random() * 10) + 1;
+
+const num3 = 24;
+const num4 = 5;
+
+const mod = num3 % num4
+
+const max = 20;
+
+Math.max(max);
+
